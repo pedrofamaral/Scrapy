57264f043317e060ec68c117129f66e956a8fa4d
Pedro Amaral <142808888+pedrofamaral@users.noreply.github.com>
2024-11-26 18:27:31 -0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,21 @@
+BOT_NAME = 'bookstore_scraper'
+
+SPIDER_MODULES = ['bookstore_scraper.spiders']
+NEWSPIDER_MODULE = 'bookstore_scraper.spiders'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+DOWNLOAD_DELAY = 3
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+   'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+   'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
